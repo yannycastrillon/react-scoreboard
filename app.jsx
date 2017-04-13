43c236b0c4137cbe76baa70a4e9f11c1b730cfa2
Yanny Castrillon <yannyandres117@gmail.com>
2017-04-13 15:28:12 -0700

    Finish high level component Application. Structure default values of the component and manage property types of each attribute.

--- app.jsx
+++ app.jsx
@@ -1,13 +1,34 @@
-function Application(){
+function Application(props){
   return(
-    <div className="application">
+    <div className="scoreboard">
       <div className="header">
-        <h1>Scoreboard</h1>
+        <h1>{props.title}</h1>
       </div>
       <div className="players">
         <div className="player">
           <div className="player-name">
-
+            Yanny Castrillon
+          </div>
+          <div className="player-score">
+            <div className="counter">
+              <button className="counter-action decrement"> - </button>
+              <div className="counter-score"> 31 </div>
+              <button className="counter-action increment"> + </button>
+            </div>
+          </div>
+        </div> 
+      </div>
+      <div className="players">
+        <div className="player">
+          <div className="player-name">
+            Alejandra Castrillon
+          </div>
+          <div className="player-score">
+            <div className="counter">
+              <button className="counter-action decrement"> - </button>
+              <div className="counter-score"> 34 </div>
+              <button className="counter-action increment"> + </button>
+            </div>
           </div>
         </div>
       </div>
@@ -15,4 +36,14 @@ function Application(){
   );
 }
 
-ReactDOM.render(<Application />, document.getElementById('container'));
+// Define type of properies that component applicaton has.
+Application.propTypes = {
+  title: React.PropTypes.string, //isRequired, // title must be a string. Must be required
+};
+
+// Define all default values properties of the application component.
+Application.defaultProps = {
+  title: "My Scoreboard",
+};
+
+ReactDOM.render(<Application/>, document.getElementById('container'));
