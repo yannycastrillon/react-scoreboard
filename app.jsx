21d1313f785fb63bbffee64197109ab74b71885c
Yanny Castrillon <yannyandres117@gmail.com>
2017-05-03 20:16:56 -0700

    Add player form and also remove functionality to the scoreboard. Add local state component to form. Create a Stats stateless component to manage the # of players and total scores. Use callback functions to bubble back to the parents notifications about a state change from the children.

--- app.jsx
+++ app.jsx
@@ -21,72 +21,126 @@ var PLAYERS = [
   },
 ];
 
+// Component in charge of adding a new component.
+const AddPlayerForm = React.createClass({
+  getInitialState:function() {
+    return{
+      name: "",
+    }
+  },
+
+  // When inputfield changes the name
+  onNameChange: function(e) {
+    console.log('onNameChange',e.target.value);
+    // sets and re-renders the variable name setState.
+    this.setState({name:e.target.value});
+  },
+
+  onSubmit: function(e) {
+    e.preventDefault();
+    // When submiting the form, must pass the state "name"
+    this.props.onAdd(this.state.name);
+    // Clear out the variable "name".
+    this.setState({name:""})
+  },
+
+  // "value" of the inputfield must be seen as the state.
+  render: function() {
+    return(
+      <div className="add-player-form">
+        <form onSubmit={this.onSubmit}>
+          <input type="text" value={this.state.name} onChange={this.onNameChange}></input>
+          <input type="submit" value="Add Player" onSubmit={this.onSubmit}></input>
+        </form>
+      </div>
+    )
+  }
+});
+
+// Define type of properies the component Header.
+Header.propTypes = {
+  title: React.PropTypes.string.isRequired, // title must be a string. Must be required
+  players: React.PropTypes.array.isRequired
+};
+function Header(props){
+  return(
+    <div className="header">
+      <Stats players={props.players}/>
+      <h1>{props.title}</h1>
+    </div>
+  );
+}
+
+Counter.propTypes = {
+  score: React.PropTypes.number.isRequired,
+  onChange: React.PropTypes.func.isRequired
+};
+function Counter (props){
+  return(
+    <div className="counter">
+      <button className="counter-action decrement" onClick={function(){props.onChange(-1);}}> - </button>
+      <div className="counter-score"> {props.score} </div>
+      <button className="counter-action increment" onClick={function(){props.onChange(+1);}}> + </button>
+    </div>
+  );
+}
+
+// Define properties of component Player.
+Player.propTypes = {
+  name: React.PropTypes.string.isRequired,
+  score: React.PropTypes.number,
+  onScoreChange: React.PropTypes.func.isRequired,
+  onRemove: React.PropTypes.func.isRequired
+}
+function Player(props){
+  return(
+    <div className="player">
+      <div className="player-name">
+        <a className="remove-player" onClick={props.onRemove}>x</a>
+        {props.name}
+      </div>
+      <div className="player-score">
+        <Counter score={props.score} onChange={props.onScoreChange}/>
+      </div>
+    </div>
+  );
+}
+
+
+Stats.propTypes = {
+  players: React.PropTypes.array.isRequired,
+}
+function Stats(props) {
+  let totalPlayers = props.players.length
+  let totalPoints = props.players.reduce(function(total, player) {
+    return total + player.score
+  }, 0)
+  return(
+    <table className="stats">
+      <tbody>
+        <tr>
+          <td>Players:</td>
+          <td>{totalPlayers}</td>
+        </tr>
+        <tr>
+          <td>Total Points:</td>
+          <td>{totalPoints}</td>
+        </tr>
+      </tbody>
+    </table>
+  )
+}
+
 // Define type of properies the component Applicaton.
 /*
 * Application takes a title, which is a string and no required.
 * Application also takes a Players property which is an array of objects with shape of:
 * name: Which is a string and required. score: Which is a number and required.
 */
-
-
-// Define type of properies the component Header.
-Header.propTypes = {
-  title: React.PropTypes.string.isRequired // title must be a string. Must be required
-};
-
-// Define properties of component Player.
-Player.propTypes = {
-  name: React.PropTypes.string.isRequired,
-  score: React.PropTypes.number
-}
-
-
-
-
-
-
-
-Counter.propTypes = {
-  score: React.PropTypes.number.isRequired
-};
-
-function Counter (props){
-  return(
-    <div className="counter">
-      <button className="counter-action decrement"> - </button>
-      <div className="counter-score"> {props.score} </div>
-      <button className="counter-action increment"> + </button>
-    </div>
-  );
-}
-
-function Header(props){
-  return(
-    <div className="header">
-      <h1>{props.title}</h1>
-    </div>
-  );
-}
-
-function Player(props){
-  return(
-    <div className="player">
-      <div className="player-name">
-        {props.name}
-      </div>
-      <div className="player-score">
-        <Counter score={props.score}/>
-      </div>
-    </div>
-  );
-}
-
-
 const Application = React.createClass({
-
   propTypes: {
     title: React.PropTypes.string,
-    // shape describe the attributes of the object property.
+    // shape describe the attributes of the object.
     initialPlayers: React.PropTypes.arrayOf(React.PropTypes.shape({
       name:  React.PropTypes.string.isRequired,
       score: React.PropTypes.number.isRequired,
@@ -108,15 +162,45 @@ const Application = React.createClass({
     };
   },
 
+  onScoreChange: function(index,delta){
+    console.log('onScoreChange ',index,delta)
+    this.state.players[index].score += delta
+    this.setState(this.state)
+  },
+
+  onPlayerAdd: function(name) {
+      console.log('Player added', name);
+      this.state.players.push({
+        name: name,
+        score:0,
+        id: this.state.players[this.state.players.length-1].id + 1
+      });
+      this.setState(this.state)
+  },
+
+  onRemovePlayer: function(index) {
+    // Removes element from an array
+    this.state.players.splice(index, 1);
+    this.setState(this.state);
+  },
+
   render: function(){
     return(
       <div className="scoreboard">
-        <Header title={this.props.title} />
+        <Header title={this.props.title} players={this.state.players}/>
         <div className="players">
-          {this.state.players.map(function(player){
-            return <Player key={player.id} name={player.name} score={player.score} />
-          })}
+          {this.state.players.map(function(player,index){
+            return (
+              <Player
+                onRemove={function(){this.onRemovePlayer(index)}.bind(this)}
+                onScoreChange={function(delta){this.onScoreChange(index,delta)}.bind(this)}
+                key={player.id}
+                name={player.name}
+                score={player.score}/>
+            );
+          }.bind(this))}
         </div>
+        <AddPlayerForm onAdd={this.onPlayerAdd}/>
       </div>
     );
   }
